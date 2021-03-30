0fb5566af6f53b5fb44e877227af652758a2d953
y1220 <ga.2.y1220@gmail.com>
2021-03-30 17:24:00 +0200

    rails s -b 0.0.0.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'products#index'
+  resources :products
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
