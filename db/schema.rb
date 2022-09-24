49314adc0dc657028dabdc1e82f3026544dcb6c5
y1220 <ga.2.y1220@gmail.com>
2022-09-24 19:40:58 +0200

    (model): add comment to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_20_213327) do
+ActiveRecord::Schema.define(version: 2022_09_24_164810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -117,6 +117,7 @@ ActiveRecord::Schema.define(version: 2022_09_20_213327) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "last_status"
+    t.text "comment"
   end
 
   add_foreign_key "checklists", "tasks"
