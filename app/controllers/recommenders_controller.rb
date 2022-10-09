88006edbb8fe9003ada834e87f29cebb18ed0550
y1220 <ga.2.y1220@gmail.com>
2022-10-09 19:16:16 +0200

    (feature): let radar plot be dynamic to visualize task similarity

--- app/controllers/recommenders_controller.rb
+++ app/controllers/recommenders_controller.rb
@@ -1,7 +1,25 @@
 class RecommendersController < ApplicationController
   def index
     @users = User.all.order("id")[0..3]
-    @tasks = User.first.tasks #NEED FIX: fetch selected user
+    @task_name_list= Task.all.last(6)
+    @scores= [
+    [60.2, 33.2, 43.6, 16.8, 25.5, 53.8, 81.2],
+    [32.6, 73.2, 92.4, 13.6, 18.2, 28.0, 22.6],
+    [82.3, 72.1, 11.3, 9.2, 18.5, 51.7, 28.3],
+    [82.6, 11.5, 27.9, 45.7, 25.9, 33.3, 51.2],
+    [82.3, 11.3, 29.3, 48.7, 24.8, 16.2, 18.7],
+    [16.3, 18.2, 26.3, 52.4, 42.7, 16.3, 36.9]
+    ]
+    @tasks = User.first.tasks.last(3) #NEED FIX: fetch selected user
+    @radar_scores= [
+      {   name: @tasks.first.name,
+        scores: [82.6, 11.5, 27.9, 45.7, 25.9, 33.3, 51.2]},
+      {   name: @tasks.second.name,
+        scores:[82.3, 11.3, 29.3, 48.7, 24.8, 16.2, 18.7]},
+      {   name: @tasks.third.name,
+        scores:[16.3, 18.2, 26.3, 52.4, 42.7, 16.3, 36.9]}
+      ]
+    @analyzed_names = ['Integration for AAA', 'Integration for BBB', 'Integration for CCC']
     @names = User.first(5).map(&:username)
     @analysis = [11, 16, 7, 3, 14] #NEED FIX: analysis need to be implemented based on availability, skill set etc
   end
