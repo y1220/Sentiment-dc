9cbb4a45198125857708553cb046fc781af34c39
fool <lucacolombo66@gmail.com>
2022-10-30 18:07:24 +0100

    first setup of ipynb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,26 +7,31 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # 1. prepare status
-['available', 'normal', 'busy'].each do |status|
-    Availability.create(name: status)
-end
+# ['available', 'normal', 'busy'].each do |status|
+#     Availability.create(name: status)
+# end
 
 # 2. prepare user
 # (https://homepage.net/name_generator/)
-{'Sue Welch' => 'FE developer', 'Abigail Henderson' => 'BE developer', 'Ryan Turner' => 'BE developer', 'Dominic Hemmings' => 'Fullstack developer', 'Karen Bell'=> 'FE developer', 'Piers Thomson' => 'Data engineer'}.each do |name, role|
-    User.create(username: name, role: role)
-end
+# {'Sue Welch' => 'FE developer', 'Abigail Henderson' => 'BE developer', 'Ryan Turner' => 'BE developer', 'Dominic Hemmings' => 'Fullstack developer', 'Karen Bell'=> 'FE developer', 'Piers Thomson' => 'Data engineer'}.each do |name, role|
+#     User.create(username: name, role: role)
+# end
 
 # 3. associate user and status
-5.times do
-  (1..User.all.count).to_a.each do |x|
-     @user = User.find(x)
-     @user.availabilities << Availability.find(rand(1..3))
-     @user.save
-  end
-end
+# 5.times do
+#   (1..User.all.count).to_a.each do |x|
+#      @user = User.find(x)
+#      @user.availabilities << Availability.find(rand(1..3))
+#      @user.save
+#   end
+# end
 
-# 4. associate user and task
+# 4. prepare tasks
+# {'First Task' => 'Awesome task on awesome app', 'Second Task' => 'More tasks on awesome app'}.each do |name, description|
+#     Task.create(name: name, description: description)
+# end
+
+# 5. associate user and task
 # 5.times do
 #   (1..User.all.count).to_a.each do |x|
 #      @user = User.find(x)
