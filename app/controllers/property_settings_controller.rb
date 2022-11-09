fdbd244be7e7dec50f4df3d0382101788317a096
y1220 <ga.2.y1220@gmail.com>
2022-11-09 22:37:18 +0100

    (fix): refine no change call error

--- app/controllers/property_settings_controller.rb
+++ app/controllers/property_settings_controller.rb
@@ -33,11 +33,13 @@ class PropertySettingsController < ApplicationController
 
   def update_key_value
     id= params.keys[2].split(/-/)[2].to_i
-    p= PropertySetting.find(id)
-    key_or_value= params.keys[2].split(/-/)[0]
-    key_or_value == 'key' ? p.key_name = params[params.keys[2]] : p.value_text = params[params.keys[2]]
-    if !p.save
-      show_error("Something went wrong..try again!","commits/index")
+    if id!=0
+      p= PropertySetting.find(id)
+      key_or_value= params.keys[2].split(/-/)[0]
+      key_or_value == 'key' ? p.key_name = params[params.keys[2]] : p.value_text = params[params.keys[2]]
+      if !p.save
+        show_error("Something went wrong..try again!","commits/index")
+      end
     end
     redirect_to("/property_settings/index")
   end
