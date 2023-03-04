026b609d2875cb84355ef211667c3957df745a75
y1220 <ga.2.y1220@gmail.com>
2023-03-04 16:02:37 +0100

    (feature): add coontributor git api

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,9 +1,41 @@
 class AdminController < ApplicationController
+
+
+  include HTTParty
+  base_uri "https://api.github.com/repos"
+
   def user_create
     @users = User.all
     @repositories = Repository.all
   end
 
   def user_update
+    repo = Repository.find(params['id'])
+    response = Repository.contributors(params['id'])
+    response.each do |contributor|
+      username = contributor['login']
+      gid = contributor['id']
+      user = User.where(gid: contributor['id'])
+      if user.count != 0
+        if !repo.users.map(&:gid).include? contributor['id']
+          repo.users << user
+        end
+      else
+        user = User.new(git_username: username, gid: gid)
+        if user.save
+          repo.users << user
+          flash[:notice]= "Saving new user has been success!"
+        else
+          show_error("Something went wrong..try again!","admin/user_create")
+        end
+      end
+    end
+    redirect_to("/admin/user_create")
+  end
+
+  private
+  def show_error (error_message, return_to_address)
+    flash[:notice]= error_message
+    render(return_to_address)
   end
 end
--- app/models/repository.rb
+++ app/models/repository.rb
@@ -2,6 +2,19 @@ class Repository < ApplicationRecord
     has_many :repositories_users
     has_many :users, through: :repositories_users
 
+    include HTTParty
+    base_uri "https://api.github.com/repos"
+
+    def self.contributors(repo_id)
+        hash= ApplicationRecord.authenticate_gitHub
+        repo = Repository.find(repo_id)
+        username= repo.owner
+        repo_name= repo.title
+
+        response = get("/#{username}/#{repo_name}/contributors", query: hash[:query], headers: hash[:headers])
+        JSON.parse(response.body)
+    end
+
     def format_created_at
         self.created_at.present? ? self.created_at.strftime('%m-%d-%Y %l:%M %p') : nil
     end
